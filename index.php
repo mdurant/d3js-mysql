19d19d1fb5f143f5bc3aae9bca4dace1eb0aaec8
MAURICIO DURAN <mauriciodurant@gmail.com>
2016-12-15 22:57:09 -0300

    pagina 1

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Practico D3JS con Mysql</title>
+
+  <script src="https://cdnjs.com/libraries/d3"></script>
+</head>
+<body>
+
+</body>
+</html>
